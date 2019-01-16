d3a54242c1a3fea9f07cd25786cb114aae5d23bc
DavidRacz90 <davidracz90@gmail.com>
2019-01-16 11:49:18 +0100

    babelloader fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 module.exports = {
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
+     
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
           }
         }
       }
@@ -20,6 +17,5 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist',
-    
   },
 };
