596e35d9cfe91b655e708a06f21b25d82c74c1bf
DavidRacz90 <davidracz90@gmail.com>
2019-01-16 15:22:04 +0100

    feat(GoldenAcornApp)working buttons

--- src/App.js
+++ src/App.js
@@ -2,30 +2,58 @@ import React, { Component } from 'react';
 import Display from './Display';
 import Button from './Button';
 
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
       amount: 0
     }
+    this.addACorn = this.addACorn.bind(this);
+    this.eatOne = this.eatOne.bind(this);
+    this.keyWatcher = this.keyWatcher.bind(this);
   }
 
-  // onChange = (event) => {
-  //   this.setState({amount: })
-  // }
+  addACorn() {
+    this.setState({
+      amount: ++this.state.amount
+    });
+  }
+
+  eatOne() {
+    this.setState({
+      amount: this.state.amount > 0 ? --this.state.amount : this.state.amount
+    });
+  }
+
+  keyWatcher(e) {
+    const keyActionPairs = {
+      38: this.addACorn,
+      40: this.eatOne
+    }
+    keyActionPairs[e.keyCode] ? keyActionPairs[e.keyCode]() : () => { };
+  }
+
 
   render() {
     const { amount } = this.state;
     return (
       <div>
         <h1>Simple Golden Acorn App</h1>
-        <Button />
-        <Display amount = {amount}/>
-        <Button />     
+        <Button
+          functions={
+            {
+              add: this.addACorn,
+              eat: this.eatOne,
+              keyWatcher: this.keyWatcher
+            }
+          }
+        />
+        <Display amount={amount} />
       </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component} from 'react';
 
-const Button = ({ amountChange }) => {
-  return (
-    <button>
-      { amountChange }
-    </button>
-  )
+
+class Button extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  componentDidMount() {
+    document.querySelector('body').addEventListener('keydown', this.props.functions.keyWatcher)
+  }
+
+  
+  render() {
+    const { amount } = this.props
+    const { add, eat } = this.props.functions
+    return (
+      <div>
+        <div>
+          <button onClick={add}>
+             Buy one
+          </button>
+        </div>
+        <div> 
+         { amount }
+        </div>
+        <div>
+          <button onClick={eat}>
+            Eat one
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Button;
