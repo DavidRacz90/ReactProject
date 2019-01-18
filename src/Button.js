9033e4ed6bc893d2d8ab295b4e9e17a55d735b72
DavidRacz90 <davidracz90@gmail.com>
2019-01-18 11:31:12 +0100

    fixing changeRequests

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 
-const Button = ({ clickEvent, name }) => {
-  return (
+const Button = ({ clickEvent, name }) => (
     <button onClick = {clickEvent}>{name}</button>
-  )
-}
+  
+)
 
 export default Button;
